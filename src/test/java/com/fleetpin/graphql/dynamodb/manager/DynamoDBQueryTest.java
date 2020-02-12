cb2fc8b5d44751fffcd8150b1e3d238f63879003
Alexander Johnston <Aliics@hotmail.com>
2020-02-12 14:05:43 +1300

    Revert test class

--- src/test/java/com/fleetpin/graphql/dynamodb/manager/DynamoDBQueryTest.java
+++ src/test/java/com/fleetpin/graphql/dynamodb/manager/DynamoDBQueryTest.java
@@ -17,157 +17,177 @@ import java.util.concurrent.ExecutionException;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import com.fleetpin.graphql.dynamodb.manager.Table;
+
 public class DynamoDBQueryTest extends DynamoDBBase {
 
 	@Test
 	public void testSimpleQuery() throws InterruptedException, ExecutionException {
 		var db = getDatabase("test");
-		
+
 		db.put(new SimpleTable("garry")).get();
 		db.put(new SimpleTable("bob")).get();
 		db.put(new SimpleTable("frank")).get();
-		
+
 		var entries = db.query(SimpleTable.class).get();
 		Assertions.assertEquals(3, entries.size());
-		
-		entries.sort(Comparator.comparing(SimpleTable::getName));
-		
-		Assertions.assertEquals("bob", entries.get(0).getName());
-		Assertions.assertEquals("frank", entries.get(1).getName());
-		Assertions.assertEquals("garry", entries.get(2).getName());
+
+		entries.sort(Comparator.comparing(a -> a.name));
+
+		Assertions.assertEquals("bob", entries.get(0).name);
+		Assertions.assertEquals("frank", entries.get(1).name);
+		Assertions.assertEquals("garry", entries.get(2).name);
 	}
-	
+
 	@Test
 	public void testTwoTablesQuery() throws InterruptedException, ExecutionException {
 		var db = getDatabase("test");
-		
+
 		db.put(new SimpleTable("garry")).get();
 		db.put(new SimpleTable("bob")).get();
 		db.put(new SimpleTable("frank")).get();
-		
+
 		db.put(new AnotherTable("ed"));
 		db.put(new AnotherTable("eddie"));
-		
+
 		var entries = db.query(SimpleTable.class).get();
 		Assertions.assertEquals(3, entries.size());
-		
-		entries.sort(Comparator.comparing(SimpleTable::getName));
-		
-		Assertions.assertEquals("bob", entries.get(0).getName());
-		Assertions.assertEquals("frank", entries.get(1).getName());
-		Assertions.assertEquals("garry", entries.get(2).getName());
-		
+
+		entries.sort(Comparator.comparing(a -> a.name));
+
+		Assertions.assertEquals("bob", entries.get(0).name);
+		Assertions.assertEquals("frank", entries.get(1).name);
+		Assertions.assertEquals("garry", entries.get(2).name);
+
 		var entriesOther = db.query(AnotherTable.class).get();
 		Assertions.assertEquals(2, entriesOther.size());
-		
-		entriesOther.sort(Comparator.comparing(AnotherTable::getName));
-		
-		Assertions.assertEquals("ed", entriesOther.get(0).getName());
-		Assertions.assertEquals("eddie", entriesOther.get(1).getName());
+
+		entriesOther.sort(Comparator.comparing(a -> a.name));
+
+		Assertions.assertEquals("ed", entriesOther.get(0).name);
+		Assertions.assertEquals("eddie", entriesOther.get(1).name);
 
 	}
-	
-	
+
+
 
 	@Test
 	public void testQueryDeleteQuery() throws InterruptedException, ExecutionException {
 		var db = getDatabase("test");
-		
+
 		db.put(new SimpleTable("garry")).get();
 		db.put(new SimpleTable("bob")).get();
 		db.put(new SimpleTable("frank")).get();
-		
+
 		var entries = db.query(SimpleTable.class).get();
 		Assertions.assertEquals(3, entries.size());
-		
-		entries.sort(Comparator.comparing(SimpleTable::getName));
-		
-		Assertions.assertEquals("bob", entries.get(0).getName());
-		Assertions.assertEquals("frank", entries.get(1).getName());
-		Assertions.assertEquals("garry", entries.get(2).getName());
-		
+
+		entries.sort(Comparator.comparing(a -> a.name));
+
+		Assertions.assertEquals("bob", entries.get(0).name);
+		Assertions.assertEquals("frank", entries.get(1).name);
+		Assertions.assertEquals("garry", entries.get(2).name);
+
 		db.delete(entries.get(1), false);
-		
+
 		entries = db.query(SimpleTable.class).get();
 		Assertions.assertEquals(2, entries.size());
-		
-		entries.sort(Comparator.comparing(SimpleTable::getName));
-		
-		Assertions.assertEquals("bob", entries.get(0).getName());
-		Assertions.assertEquals("garry", entries.get(1).getName());
-		
+
+		entries.sort(Comparator.comparing(a -> a.name));
+
+		Assertions.assertEquals("bob", entries.get(0).name);
+		Assertions.assertEquals("garry", entries.get(1).name);
+
 	}
 
-	
+
 	@Test
 	public void testClimbingQuery() throws InterruptedException, ExecutionException {
 		var db = getDatabase("test");
 		var prod = getDatabaseProduction("test");
-		
-		
+
+
 		var garry = prod.put(new SimpleTable("garry")).get();
 		var garryLocal = new SimpleTable("GARRY");
 		garryLocal.setId(garry.getId());
 		db.put(garryLocal);
 		prod.put(new SimpleTable("bob")).get();
 		db.put(new SimpleTable("frank")).get();
-		
+
 		var entries = db.query(SimpleTable.class).get();
 		Assertions.assertEquals(3, entries.size());
-		
-		entries.sort(Comparator.comparing(SimpleTable::getName));
-		
-		Assertions.assertEquals("GARRY", entries.get(0).getName());
-		Assertions.assertEquals("bob", entries.get(1).getName());
-		Assertions.assertEquals("frank", entries.get(2).getName());
-		
+
+		entries.sort(Comparator.comparing(a -> a.name));
+
+		Assertions.assertEquals("GARRY", entries.get(0).name);
+		Assertions.assertEquals("bob", entries.get(1).name);
+		Assertions.assertEquals("frank", entries.get(2).name);
+
 		db.delete(entries.get(1), false);
-		
+
 		entries = db.query(SimpleTable.class).get();
 		Assertions.assertEquals(2, entries.size());
-		
-		entries.sort(Comparator.comparing(SimpleTable::getName));
-		
-		Assertions.assertEquals("GARRY", entries.get(0).getName());
-		Assertions.assertEquals("frank", entries.get(1).getName());
-		
+
+		entries.sort(Comparator.comparing(a -> a.name));
+
+		Assertions.assertEquals("GARRY", entries.get(0).name);
+		Assertions.assertEquals("frank", entries.get(1).name);
+
 	}
-	
+
 	@Test
 	public void testClimbingGlobalQuery() throws InterruptedException, ExecutionException {
 		var db = getDatabase("test");
 		var prod = getDatabaseProduction("test");
-		
-		
+
+
 		var garry = prod.put(new SimpleTable("garry")).get();
 		var garryLocal = new SimpleTable("GARRY");
 		garryLocal.setId(garry.getId());
 		db.put(garryLocal);
 		prod.put(new SimpleTable("bob")).get();
 		db.putGlobal(new SimpleTable("frank")).get();
-		
+
 		var entries = db.query(SimpleTable.class).get();
 		Assertions.assertEquals(3, entries.size());
-		
-		entries.sort(Comparator.comparing(SimpleTable::getName));
-		
-		Assertions.assertEquals("GARRY", entries.get(0).getName());
-		Assertions.assertEquals("bob", entries.get(1).getName());
-		Assertions.assertEquals("frank", entries.get(2).getName());
-		
+
+		entries.sort(Comparator.comparing(a -> a.name));
+
+		Assertions.assertEquals("GARRY", entries.get(0).name);
+		Assertions.assertEquals("bob", entries.get(1).name);
+		Assertions.assertEquals("frank", entries.get(2).name);
+
 		db.delete(entries.get(1), false);
-		
+
 		entries = db.query(SimpleTable.class).get();
 		Assertions.assertEquals(2, entries.size());
-		
-		entries.sort(Comparator.comparing(SimpleTable::getName));
-		
-		Assertions.assertEquals("GARRY", entries.get(0).getName());
-		Assertions.assertEquals("frank", entries.get(1).getName());
+
+		entries.sort(Comparator.comparing(a -> a.name));
+
+		Assertions.assertEquals("GARRY", entries.get(0).name);
+		Assertions.assertEquals("frank", entries.get(1).name);
+	}
+
+	static class SimpleTable extends Table {
+		private String name;
+
+		public SimpleTable() {
+		}
+
+		public SimpleTable(String name) {
+			this.name = name;
+		}
+
+		public String getName() {
+			return name;
+		}
 	}
 
 	static class AnotherTable extends Table {
 		private String name;
+
+		public AnotherTable() {
+		}
 
 		public AnotherTable(String name) {
 			this.name = name;
@@ -178,15 +198,4 @@ public class DynamoDBQueryTest extends DynamoDBBase {
 		}
 	}
 
-	static final class SimpleTable extends Table {
-		private String name;
-
-		public SimpleTable(final String name) {
-			this.name = name;
-		}
-
-		public String getName() {
-			return name;
-		}
-	}
 }
